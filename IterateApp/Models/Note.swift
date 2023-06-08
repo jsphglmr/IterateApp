18ade266f34a77cdad0787ae69dd7798d6a760c4
Jojo <jsphglmr@gmail.com>
2023-06-08 15:22:44 -0700

    add description field to note model

--- IterateApp/Models/Note.swift
+++ IterateApp/Models/Note.swift
@@ -10,6 +10,7 @@ import Foundation
 struct Note: Codable, Identifiable, Equatable {
     var id: UUID
     var title: String
+    var description: String
     var symbol: String
     var body: String
     var creationDate: Date
@@ -29,89 +30,103 @@ extension Note {
 /// Test Note Data
 extension Note {
     
-    static public let testNote = Note(id: UUID(), title: "Note1 Title", symbol: "pencil", body: "Note1 Body", creationDate: Date())
+    static public let testNote = Note(id: UUID(), title: "Note1 Title", description: "Test description", symbol: "pencil", body: "Note1 Body", creationDate: Date())
     static public let testNoteArray = [
         
             Note(id: UUID(),
                  title: "Note1 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note1 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note2 Long Title Test Test Test Test Test Test Test",
+                 description: "Test long long long description Test Test Test Test Test Test Test",
                  symbol: "pencil.circle.fill",
                  body: "Note2 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note3 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note3 Body",
                  creationDate: Date()),
         
             Note(id: UUID(),
                  title: "Note4 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note4 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note5 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note5 Body",
                  creationDate: Date()),
 
             Note(id: UUID(),
                  title: "Note6 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note6 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note7 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note7 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note8 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note8 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note9 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note9 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note10 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note10 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note11 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note11 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note12 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note12 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note13 Title",
+                 description: "Test description",
                  symbol: "pencil",
                  body: "Note13 Body",
                  creationDate: Date()),
             
             Note(id: UUID(),
                  title: "Note14 Title",
+                 description: "Test description",
                  symbol: "pencil.circle.fill",
                  body: "Note14 Body",
                  creationDate: Date())

ef569b0344be6f70d19b696efa051683974f4136
Jojo <jsphglmr@gmail.com>
2023-06-26 22:01:10 -0700

    fix test data, add date formatter

--- IterateApp/Models/Note.swift
+++ IterateApp/Models/Note.swift
@@ -10,17 +10,28 @@ import Foundation
 struct Note: Codable, Identifiable, Equatable {
     var id: UUID
     var body: String
-    var date: Date
+    var creationDate: Date
+}
+
+///Computed Properties
+extension Note {
+    var formattedDate: String {
+        let newDate = creationDate.formatted(
+            .dateTime
+                .month(.abbreviated).day(.defaultDigits).year(.twoDigits)
+                .hour(.defaultDigits(amPM: .abbreviated)))
+        return newDate
+    }
 }
 
 ///Test Note Bodies
 extension Note {
-    static public let testBody = Note(id: UUID(), body: "Test body", date: Date.now)
+    static public let testBody = Note(id: UUID(), body: "Test body", creationDate: Date.now)
     static public let testBodyArray = [
-        Note(id: UUID(), body: "Test body1", date: Date.now),
-        Note(id: UUID(), body: "Test body2", date: Date.now),
-        Note(id: UUID(), body: "Test body3", date: Date.now),
-        Note(id: UUID(), body: "Test body4", date: Date.now),
-        Note(id: UUID(), body: "Test body5", date: Date.now)
+        Note(id: UUID(), body: "Test body1", creationDate: Date.now),
+        Note(id: UUID(), body: "Test body2", creationDate: Date.now),
+        Note(id: UUID(), body: "Test body3", creationDate: Date.now),
+        Note(id: UUID(), body: "Test body4", creationDate: Date.now),
+        Note(id: UUID(), body: "Test body5", creationDate: Date.now)
     ]
 }

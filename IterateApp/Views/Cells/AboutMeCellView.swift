4afdaece2ac007e3e26940dab7cb3353aed7b1c1
Joseph Gilmore <jsphglmr@gmail.com>
2024-04-27 22:34:11 -0700

    add custom about me cell view

---
+++ IterateApp/Views/Cells/AboutMeCellView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutMeCellView.swift
+//  IterateApp
+//
+//  Created by Joseph Gilmore on 4/27/24.
+//
+
+import SwiftUI
+
+struct AboutMeCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutMeCellView()
+}
